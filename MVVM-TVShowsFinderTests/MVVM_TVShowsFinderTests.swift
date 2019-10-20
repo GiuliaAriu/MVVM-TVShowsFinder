ba8ed4a19efe4e1bde9680f2c7077b0be8011db5
GiuliaAriu <giuulia.ariu@gmail.com>
2019-10-20 15:36:14 +0200

    Initial Commit

---
+++ MVVM-TVShowsFinderTests/MVVM_TVShowsFinderTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_TVShowsFinderTests.swift
+//  MVVM-TVShowsFinderTests
+//
+//  Created by Giulia Ariu on 20/10/2019.
+//  Copyright © 2019 Giulia Ariu. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_TVShowsFinder
+
+class MVVM_TVShowsFinderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
